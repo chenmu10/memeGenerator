c3ff97ec7efb8425b07b9a550f29d3392ba3377e
Chen Mualem <chenmu10@gmail.com>
2018-03-21 16:40:38 +0200

    reset gMeme, added change color,align,size and hidden class toggle

--- js/meme.js
+++ js/meme.js
@@ -2,42 +2,47 @@
 
 
 
-var gMeme = {
-    selectedImgId: null,
-    txts: [
-        {
-            line: 'Line 1',
-            size: 30,
-            align: 'left',
-            color: 'red',
-            fontFamily: 'Segoe UI',
-            coord: { x: 150, y: 70 }
-        },
-        {
-            line: 'Line 2',
-            size: 30,
-            align: 'left',
-            color: 'red',
-            fontFamily: 'Segoe UI',
-            coord: { x: 150, y: 300 }
-        }
-    ]
-};
+var gMeme;
 var gCtx;
 var gImgObj;
 
+function resetgMeme(imgId) {
+    gMeme = {
+        selectedImgId: imgId,
+        txts: [
+            {
+                line: 'Line 1',
+                size: 30,
+                align: 'left',
+                color: 'red',
+                fontFamily: 'Segoe UI',
+                coord: { x: 150, y: 70 }
+            },
+            {
+                line: 'Line 2',
+                size: 30,
+                align: 'left',
+                color: 'red',
+                fontFamily: 'Segoe UI',
+                coord: { x: 150, y: 300 }
+            }
+        ]
+    };
+}
+
 function initImgCanvas(imgId) {
-    gMeme.selectedImgId = imgId;
-    
-    
-    var canvas= document.getElementById('memeCanvas');
+    toggleView();
+    resetgMeme(imgId);
+
+
+    var canvas = document.getElementById('memeCanvas');
     gCtx = canvas.getContext('2d');
 
     gImgObj = new Image();
     gImgObj.onload = function () {
         canvas.width = gImgObj.width;
         canvas.height = gImgObj.height;
-        gMeme.txts[1].coord.y = gImgObj.height-70;
+        gMeme.txts[1].coord.y = gImgObj.height - 70;
 
         drawCanvas();
     };
@@ -76,25 +81,36 @@ function drawImg(imageObj) {
 
 
 
-function editTxt(eltxt, lineNum) {
+function editTxt(elinput, lineNum) {
     var txtIdx = lineNum - 1;
-    var property = eltxt.name;  
+    var property = elinput.id;
 
-    gMeme.txts[txtIdx][property] = eltxt.value;
+    if (elinput.type === 'select-one') {
+        gMeme.txts[txtIdx][property] = elinput.options[elinput.selectedIndex].value;
+    } else {
+        gMeme.txts[txtIdx][property] = elinput.value;
+    }
+
+
     drawCanvas();
 }
 
 
 /* REGISTER DOWNLOAD HANDLER */
-   function dlCanvas(eldllink) {
+function dlCanvas(eldllink) {
     var canvas = document.getElementById('memeCanvas');
-  
+
     var dt = canvas.toDataURL('image/png');
     /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
     dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
-  
+
     /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
     dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
-  
+
     eldllink.href = dt;
-  };
+};
+
+function toggleView() {
+    document.querySelector('.meme-container').classList.toggle('hidden');
+    document.querySelector('.gallery').classList.toggle('hidden');
+}
